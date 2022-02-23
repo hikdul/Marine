9a3207fead74875f846f7db2ab3930aa12e7eb43
hikdul <hikdul.lio@gmail.com>
2022-02-22 22:09:56 -0500

    solo falta actulisar los equipos para terminar este controlador

--- Marine/Controllers/EquipoController.cs
+++ Marine/Controllers/EquipoController.cs
@@ -4,6 +4,7 @@ using Marine.DTOs;
 using Marine.Entitys;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Marine.Controllers
 {
@@ -79,7 +80,71 @@ namespace Marine.Controllers
 
         #endregion
 
+        #region obtener
 
+        /// <summary>
+        /// para obtener una lista de los equipo activos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                var turno = await context.Turnos.Where(x => x.act == true).ToListAsync();
+
+                if(turno == null || turno.Count == 0)
+                    return NoContent();
+                var ent = await context.Equipos
+                    .Include( x => x.Cargo)
+                    .ToListAsync();
+
+                var retorno = EquipoDTO_out.Up(ent, turno);
+
+                return Ok(retorno);
+            
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return BadRequest("upss, algo salio mal");
+            }
+        }
+
+        /// <summary>
+        /// para obtener los detalles de un equipo en particular,
+        /// por medio del id de turno
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            try
+            {
+                var turno = await context.Turnos.Where(x => x.id == id).FirstOrDefaultAsync();
+                var ent = await context.Equipos
+                    .Include(x => x.Cargo)
+                    .Where(x => x.Turnoid == id)
+                    .ToListAsync();
+                if (turno == null || ent == null || ent.Count == 0)
+                    return BadRequest("Datos no Validos");
+
+
+                var retorno = new EquipoDTO_out(ent,turno);
+
+                return Ok(retorno);
+
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return BadRequest("upss, algo salio mal");
+            }
+        }
+
+
+        #endregion
 
 
     }
